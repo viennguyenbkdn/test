ac1a02080a26aaffcf7476ba8340c8cb746f0753
viennguyenbkdn <viennguyenbkdn@gmail.com>
2022-09-20 12:58:23 +0200

    haqq

---
+++ src/@core/layouts/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg
+    id="Layer_1"
+    version="1.1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    width="150px"
+    height="150px"
+    viewBox="0 0 150 150"
+    enable-background="new 0 0 150 150"
+    xml:space="preserve"
+  >  <image
+    id="image0"
+    width="150"
+    height="150"
+    x="0"
+    y="0"
+    href="https://i.imgur.com/pBRmmiE.jpg"
+  />
+  </svg>
+</template>
